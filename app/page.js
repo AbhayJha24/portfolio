d384ce74e501aa1217e754853017464139390e62
Abhay <abhay.jha243@gmail.com>
2023-12-10 21:44:09 +0530

    Inital Commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,26 @@
 import Image from 'next/image'
+import photo from '../public/photo.jpg'
 import styles from './page.module.css'
 
 export default function Home() {
+
+  let profession = "Professional";
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={styles.hero}>
+      <section className={styles.titleSection}>
+        <h2>Hello, It's me</h2>
+        <h1 className={styles.titlename}>Abhay Jha</h1>
+        <h2 className={styles.herotagline}>And I'm a <span className={styles.titleblue}>{profession}</span></h2>
+        <p className={styles.herodesc}>Esse deserunt et aliquip irure. Pariatur ad reprehenderit ullamco esse consequat incididunt q.</p>
+        <button className={styles.learnmore}>Know More</button>
+      </section>
+      <section className={styles.photoSection}>
+        <div className={styles.photobg1}></div>
+        <div className={styles.photobg2}></div>
+        <div className={styles.photoframe}>
+        <Image src={photo} alt='Image' width={300} height={300} className={styles.photo}></Image>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
   )
 }
