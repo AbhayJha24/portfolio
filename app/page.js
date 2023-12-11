e21c26910488d9a5a32d11cac096985dea9b7054
Abhay <abhay.jha243@gmail.com>
2023-12-11 10:43:55 +0530

    Made Responsive till 260px phone width

--- app/page.js
+++ app/page.js
@@ -3,14 +3,13 @@ import photo from '../public/photo.webp'
 import styles from './page.module.css'
 
 export default function Home() {
-
-  let profession = "Professional";
+  
   return (
     <main className={styles.hero}>
       <section className={styles.titleSection}>
         <h2>Hello, It's me</h2>
         <h1 className={styles.titlename}>Abhay Jha</h1>
-        <h2 className={styles.herotagline}>And I'm a <span className={styles.titleblue}>{profession}</span></h2>
+        <h2 className={styles.herotagline}>And I'm a <span className={styles.titleblue}>Professional</span></h2>
         <p className={styles.herodesc}>Esse deserunt et aliquip irure. Pariatur ad reprehenderit ullamco esse consequat incididunt q.</p>
         <button className={styles.learnmore}>Know More</button>
       </section>
@@ -18,7 +17,7 @@ export default function Home() {
         <div className={styles.photobg1}></div>
         <div className={styles.photobg2}></div>
         <div className={styles.photoframe}>
-        <Image src={photo} alt='Image' width={300} height={300} className={styles.photo}></Image>
+        <Image src={photo} alt='Image' className={styles.photo}></Image>
         </div>
       </section>
     </main>
